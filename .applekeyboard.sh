1a45c94643f926f43c8537730f7de88245cafea2
Henrik Heggelund-Berg <henrik.heggelund.berg@indusenz.com>
2020-05-15 21:27:02 +0200

    Add keyboard mapping files

---
+++ .applekeyboard.sh
@@ -0,0 +1,2 @@
+setxkbmap -option apple:badmap
+sudo bash -c 'echo "1" > /sys/module/hid_apple/parameters/swap_opt_cmd'
---
+++ .regularkeyboard.sh
@@ -0,0 +1,2 @@
+setxkbmap -option
+sudo bash -c 'echo "0" > /sys/module/hid_apple/parameters/swap_opt_cmd'
